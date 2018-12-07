83232885bfaddacaee8204b2b82ece0d0ff7b3ad
Accuratemiss <jimboock1@live.co.uk>
2018-12-07 16:28:58 +0000

    fixed interfaced.py

--- interfaced.py
+++ interfaced.py
@@ -29,19 +29,28 @@ mech1 = int(mech1)
 mech2 = input('Please enter the number of mech2 instances: ')
 mech2 = int(mech2)
 
+# num_runs = 3
+# name = 'tested.gin'
+#
+# mech1 = 1
+# mech2 = 2
 num_table = []
 
 
 def mech_Ti_Ba(Ba_sites, Ti_sites):
     #carries out Ti/Ba substitution
     for x in Ba_sites:
-        for x in Ba_sites:
-            new_lat.replaceatom(x.pos,'Er')
-            num_table.append(str(n) + ' ' + str(x) + ' ' + str(x.type))
+        # print('yo')
+        new_lat.replaceatom(x.pos, 'Er')
+        num_table.append(str(n) + ' ' + str(x) + ' ' + str(x.type))
+        # print(x.type)
 
-        for x in Ti_sites:
-            lat.replaceatom(x.pos, 'Er')
-            num_table.append(str(n) + ' ' + str(x) + ' ' + str(x.type))
+    for x in Ti_sites:
+        # print('hi')
+        # print(x.type)
+
+        new_lat.replaceatom(x.pos, 'Er')
+        num_table.append(str(n) + ' ' + str(x) + ' ' + str(x.type))
         return
 
 def mech_Ti_Ov(Ti_sites, O_sites):
@@ -49,11 +58,16 @@ def mech_Ti_Ov(Ti_sites, O_sites):
         new_lat.replaceatom(x.pos, 'Er')
         num_table.append(str(n) + ' ' + str(x) + ' ' + str(x.type))
     for x in O_sites:
-        lat.replaceatom(x.pos, 'O_v')
+        new_lat.replaceatom(x.pos, 'O_v')
+        num_table.append(str(n) + ' ' + str(x) + ' ' + str(x.type))
+
     return
 
 
-
+def errorcharge(lat):
+    if cg.check_charge(lat) != 0:
+        raise Exception('Bad!!!')
+    return
 
 while n <= num_runs:
     new_lat = copy.deepcopy(lat)
@@ -68,10 +82,22 @@ while n <= num_runs:
     O_master = random.sample(O_list, O_num)
 
     if not mech1 == 0:
-        mech_Ti_Ba(Ba_master[0:mech1 - 1], Ti_master[0: mech1-1])
+        mech_Ti_Ba(Ba_master[0:mech1], Ti_master[0: mech1])
 
     if not mech2 == 0:
         mech_Ti_Ov(Ti_master[mech1:], O_master)
+
+    try:
+        errorcharge(new_lat)
+    except:
+        print(cg.check_charge(new_lat))
+        file = open(name + 'pos_error', 'w+')
+        for x in num_table:
+            file.write(str(x)+'\n')
+        file.write(str(cg.check_charge(new_lat)))
+        print('bad!!')
+        break
+
 
     cg.genwholefile(new_lat, name + str(n))
     n += 1
